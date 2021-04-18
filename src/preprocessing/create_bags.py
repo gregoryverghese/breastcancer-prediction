2b40542f1a562255064120a63ae7b4b3cc1cb4b1
gregory.verghese@gmail.com <verghese@gamble.cs.ucl.ac.uk>
2021-04-18 21:36:04 +0100

    created pytorch dataset to generate bags from bresatcancer images

--- src/preprocessing/create_bags.py
+++ src/preprocessing/create_bags.py
@@ -9,13 +9,12 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 
-class PatchBags(Dataset):
+class ICIARBags(Dataset):
     def __init__(self,patch_path,labels,n=200,key={},name='labels'):
         self.n_size=n
         self.name=name
         self.key=key
         self.patch_paths=glob.glob(os.path.join(patch_path,'*'))
-        self.labels=labels
         self.patch_num=len(self.patch_paths)
         self.patch_bags,self.label_bags=self._generate_bags()
         self.positive_num=len(list(filter(lambda x: x==1, self.label_bags)))
@@ -26,26 +25,19 @@ class PatchBags(Dataset):
         
         train_patch_bags=[]
         train_label_bags=[]
-        patches=[p for p in self.patch_paths]
-        bag_names=list(self.labels.index)
-        for i, b in enumerate(bag_names):
-            image_patches=[p for p in patches if b in p]
-            bag_size=self.n_size if len(image_patches)>self.n_size else len(image_patches)
-            if bag_size==0:
-                continue
-            if bag_size<len(image_patches):
-                num_bags=int(np.floor(len(image_patches)/bag_size))
-            else:
-                num_bags=1
-            label_bag=[self.labels.loc[b][self.name]]
-            label_bag=[self.key[l] for l in label_bag]
-            for bi in range(num_bags):
-                bag=random.sample(image_patches,bag_size)
-                train_patch_bags.append(bag)
-                train_label_bags.append(torch.tensor(label_bag))
-        return train_patch_bags,train_label_bags
-            
-            
+        image_paths=glob.glob(os.path.join(self.patch_paths,'*'))
+        for i, path in enumerate(image_paths):
+            class_paths=glob.glob(os.path.join(path,'*'))
+            train_label_bags.append(i)
+            for c in class_paths:
+                image=cv2.imread(c)
+                patches=[image[i:i+28,j:j+28,:] for i in
+                        range(0,28*16,28) for j in range(0,28*25,28)])
+                train_patch_bags.append(torch.tensor(patches))
+
+        return train_patch_bags, train_label_bags
+
+
     def __len__(self):
         return len(self.patch_bags)
     
